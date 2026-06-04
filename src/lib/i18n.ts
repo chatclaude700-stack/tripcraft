493a4059f4d368eaeeb8fc6d4386604e0e241095
chatclaude700-stack <chatclaude700@gmail.com>
2026-06-04 11:49:17 +0500

    Add i18n configuration

---
+++ src/lib/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import enTranslations from './en.json'
+import ruTranslations from './ru.json'
+import uzTranslations from './uz.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslations },
+      ru: { translation: ruTranslations },
+      uz: { translation: uzTranslations },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
